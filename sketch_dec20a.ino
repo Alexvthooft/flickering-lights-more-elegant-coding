0e6aa10f41d519d68c55619a9cf97d9e2d7b23cd
Alexvthooft <Alexvthooft@gmail.com>
2016-12-20 10:49:34 +0100

    Add files via upload
    My question is:
    
    how to create a more elegant (but not too difficult) solution for the volume of code used to get the lights to blink.
    90 rules seems excessive. 
    Please take into account that this is about the level I am at when it comes to programming.

---
+++ sketch_dec20a.ino
@@ -0,0 +1,95 @@
+const int blueLed = 8;
+const int redLed = 9;
+
+void setup()
+{
+  pinMode(blueLed, OUTPUT);
+  pinMode(redLed, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+  digitalWrite(blueLed, HIGH);
+  delay(50);
+  digitalWrite(blueLed, LOW);
+  delay(50);
+
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+  digitalWrite(redLed, HIGH);
+  delay(50);
+  digitalWrite(redLed, LOW);
+  delay(50);
+
+}
+
